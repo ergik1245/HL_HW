f836e22727258755bfd98230900acdc8d05a6a21
Strokov Vadym <i.munstang95.i@gmail.com>
2026-07-19 20:12:32 +0200

    fixed not-pythonic moments, changed tuple, changed check_data, changed total

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,5 +1,4 @@
-months: tuple[str] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
+months: list[str] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 
 def get_data():
@@ -8,15 +7,15 @@ def get_data():
     return data
 
 
-
-def check_data(data):
+def prepare_data(data):
     if len(data) != 12:
         print("12, Карл, их должно быть 12 а не", len(data))
         return False
 
-    for i in range(len(data)):
+    numbers = []
+    for value in data:
         try:
-            number = float(data[i])
+            number = float(value)
         except:
             print("Принимаем только числами")
             return False
@@ -25,54 +24,49 @@ def check_data(data):
             print("Партия запрещает отрицательные осадки, переделывай")
             return False
 
-    return True
+        numbers.append(number)
+
+    return numbers
 
 
-
-def calculate(data):
-    numbers = []
-    for i in range(len(data)):
-        numbers.append(float(data[i]))
-
-    total = 0
+def calculate(numbers):
+    sum = 0
     for n in numbers:
-        total = total + n
+        sum = sum + n
 
-    average = total / 12
+    average = sum / 12
 
     max_value = numbers[0]
     max_index = 0
     min_value = numbers[0]
     min_index = 0
 
-    for i in range(len(numbers)):
-        if numbers[i] > max_value:
-            max_value = numbers[i]
+    for i, number in enumerate(numbers):
+        if number > max_value:
+            max_value = number
             max_index = i
-        if numbers[i] < min_value:
-            min_value = numbers[i]
+        if number < min_value:
+            min_value = number
             min_index = i
 
     max_month = months[max_index]
     min_month = months[min_index]
 
-    result = (total, average, (max_value, max_month), (min_value, min_month))
+    result = (sum, average, (max_value, max_month), (min_value, min_month))
     return result
-
 
 
 def show_result(result):
     print(result)
 
 
-
 def main():
-    ok = False
-    while ok == False:
+    numbers = False
+    while numbers == False:
         info = get_data()
-        ok = check_data(info)
+        numbers = prepare_data(info)
 
-    result = calculate(info)
+    result = calculate(numbers)
     show_result(result)
 
 
